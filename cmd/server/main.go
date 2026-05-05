c779ad550b54df1a92cfbfa7640ae8cbf25c48d1
Narathip Santhip <narathip.sa@beryl8.com>
2026-05-05 18:16:28 +0700

    refactor: modularize fx application structure and improve Redis connection handling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -13,11 +13,8 @@ import (
 	"github.com/joho/godotenv"
 	"go.uber.org/fx"
 
-	deliveryservice "kbank-ecms/cmd/server/service"
 	"kbank-ecms/internal/domain/entity"
-	domainrepo "kbank-ecms/internal/domain/repository"
 	"kbank-ecms/internal/infrastructure/logger"
-	"kbank-ecms/internal/repository"
 )
 
 func main() {
@@ -26,51 +23,7 @@ func main() {
 		Service: "CMS-DELIVERY", Level: "INFO", Message: "Starting cms-delivery pod",
 	})
 
-	app := fx.New(
-		fx.Provide(
-			ProvideConfig,
-			ProvidePostgresDB,
-			ProvideRedisRepository,
-
-			// Repository interface bindings
-			fx.Annotate(repository.NewScheduleOccurrencePostgresRepository,
-				fx.As(new(domainrepo.ScheduleOccurrenceRepository))),
-			fx.Annotate(repository.NewDecisionRulePostgresRepository,
-				fx.As(new(domainrepo.DecisionRuleRepository))),
-			fx.Annotate(repository.NewCLENLeadRepository,
-				fx.As(new(domainrepo.LeadRepository))),
-			fx.Annotate(repository.NewCLENCustomerProfileRepository,
-				fx.As(new(domainrepo.CustomerProfileRepository))),
-			fx.Annotate(repository.NewCLENSchemaRegistryPostgresRepository,
-				fx.As(new(domainrepo.CLENSchemaRegistryRepository))),
-			fx.Annotate(repository.NewAttributePostgresRepository,
-				fx.As(new(domainrepo.AttributeRepository))),
-
-			// RedisCacheRepository satisfied from the same *RedisRepository singleton
-			func(r *repository.RedisRepository) domainrepo.RedisCacheRepository { return r },
-
-			// CLEN clients + configs
-			ProvideCLENLeadConfig,
-			ProvideCLENLeadClient,
-			ProvideCLENCustomerProfileConfig,
-			ProvideCLENCustomerProfileClient,
-			ProvideCustomerProfileEnrichConfig,
-
-			// Core
-			ProvideCacheMemory,
-			fx.Annotate(ProvideRuntimeEvaluator,
-				fx.As(new(deliveryservice.RuntimeEvaluator))),
-			ProvideCMSDeliveryService,
-			// DeliveryService interface satisfied from the same *CMSDeliveryService singleton
-			func(s *deliveryservice.CMSDeliveryService) deliveryservice.DeliveryService { return s },
-			ProvideRouter,
-		),
-		fx.Invoke(
-			RegisterSwaggerHost,
-			RegisterServiceLifecycle,
-			RegisterHTTPServer,
-		),
-	)
+	app := fx.New(App())
 
 	// app.Err() catches DI graph construction errors (from fx.New) before app.Run() is called.
 	if err := app.Err(); err != nil {
