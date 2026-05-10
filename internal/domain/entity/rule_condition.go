de0c510c9db1dfc742ecf335db9459490dc81fd7
nrtsa <narathip.sa@beryl8.com>
2026-05-10 14:50:04 +0700

    docs: update RuleCondition struct to include optional childConnectorOperator
    - Changed ConnectorOperator to be a pointer to allow nil values.
    - Added ChildConnectorOperator as an optional field to enhance rule condition flexibility.

--- internal/domain/entity/rule_condition.go
+++ internal/domain/entity/rule_condition.go
@@ -12,16 +12,17 @@ import (
 // Table: rule_conditions
 type RuleCondition struct {
 	BaseModel
-	Sequence              int                     `gorm:"type:integer"                                                                   json:"sequence"`
-	DecisionRuleID        uuid.UUID               `gorm:"type:uuid;not null"                                                             json:"decisionRuleId"`
-	DecisionRule          *DecisionRule           `gorm:"foreignKey:DecisionRuleID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"        json:"decisionRule,omitempty"`
-	ParentRuleConditionID *uuid.UUID              `gorm:"type:uuid"                                                                      json:"parentRuleConditionId"`
-	ParentRuleCondition   *RuleCondition          `gorm:"foreignKey:ParentRuleConditionID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"parentRuleCondition,omitempty"`
-	RuleConditionChildren []RuleCondition         `gorm:"foreignKey:ParentRuleConditionID;references:ID"                                 json:"ruleConditionChildren,omitempty"`
-	AttributeID           uuid.UUID               `gorm:"type:uuid;not null"                                                             json:"attributeId"`
-	Attribute             *Attribute              `gorm:"foreignKey:AttributeID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"           json:"attribute,omitempty"`
-	LogicalOperator       enums.LogicalOperator   `gorm:"size:50"                                                                        json:"logicalOperator"`
-	ConnectorOperator     enums.ConnectorOperator `gorm:"size:50"                                                                        json:"connectorOperator"`
+	Sequence               int                      `gorm:"type:integer"                                                                   json:"sequence"`
+	DecisionRuleID         uuid.UUID                `gorm:"type:uuid;not null"                                                             json:"decisionRuleId"`
+	DecisionRule           *DecisionRule            `gorm:"foreignKey:DecisionRuleID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"        json:"decisionRule,omitempty"`
+	ParentRuleConditionID  *uuid.UUID               `gorm:"type:uuid"                                                                      json:"parentRuleConditionId"`
+	ParentRuleCondition    *RuleCondition           `gorm:"foreignKey:ParentRuleConditionID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"parentRuleCondition,omitempty"`
+	RuleConditionChildren  []RuleCondition          `gorm:"foreignKey:ParentRuleConditionID;references:ID"                                 json:"ruleConditionChildren,omitempty"`
+	AttributeID            uuid.UUID                `gorm:"type:uuid;not null"                                                             json:"attributeId"`
+	Attribute              *Attribute               `gorm:"foreignKey:AttributeID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"           json:"attribute,omitempty"`
+	LogicalOperator        enums.LogicalOperator    `gorm:"size:50"                                                                        json:"logicalOperator"`
+	ConnectorOperator      *enums.ConnectorOperator `gorm:"size:50"                                                                        json:"connectorOperator"`
+	ChildConnectorOperator *enums.ConnectorOperator `gorm:"size:50"                                                                        json:"childConnectorOperator"`
 }
 
 // TableName overrides default GORM table name to use plural form matching DBML.
